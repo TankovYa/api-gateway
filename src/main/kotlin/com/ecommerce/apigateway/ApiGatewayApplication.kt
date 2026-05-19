1a966fd1d1b63254013ff590c1dba9f27c4d9ce0
TankovYa <tankov.ya@gmail.com>
2026-05-19 23:06:30 +0300

    fix

--- src/main/kotlin/com/ecommerce/apigateway/ApiGatewayApplication.kt
+++ src/main/kotlin/com/ecommerce/apigateway/ApiGatewayApplication.kt
@@ -2,7 +2,6 @@ package com.ecommerce.apigateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
 class ApiGatewayApplication
